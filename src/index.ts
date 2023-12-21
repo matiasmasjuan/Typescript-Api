a69b030e8821e12e3bfe8dff9e8d9285d65b1e73
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-21 00:35:26 -0300

    :sparkles: Basic User Routes

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,15 @@ import bodyParser from 'body-parser';
 import { Request, Response } from 'express';
 import { port } from './config';
 
+import usersRoutes from './routes/users.routes';
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/users', usersRoutes);
 
 app.get('/', async (req: Request, res: Response) => {
   res.status(200).json({
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,19 @@
+import { Router, Request, Response } from 'express';
+const router = Router();
+
+// GET - users
+router.get('/', async (req: Request, res: Response) => {
+  const result : string[] = [];
+  res.status(200).json({ users: result });
+});
+
+// GET - users/:id
+router.get('/:id', async (req: Request, res: Response) => {
+  const result : string = '';
+  res.status(200).json({ user: result });
+});
+
+// POST - users
+router.post('/', async (req: Request, res: Response) => {
+  res.status(201).json({ user: {} });
+});export default router;
