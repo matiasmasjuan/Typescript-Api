bb1f9722640210b364f893109f9e9346d248c3dd
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-21 00:44:42 -0300

    :ballot_box: Add Sequelize and db config

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,9 @@ dotenv.config({
   path: path.join(__dirname, '../.env')
 });
 
+export const db_host = String(process.env.DB_HOST);
+export const db_port = Number(process.env.DB_PORT);
+export const db_name = String(process.env.DB_NAME);
+export const db_user = String(process.env.DB_USER);
+export const db_password = String(process.env.DB_PASSWORD);
 export const port = Number(process.env.API_PORT);
