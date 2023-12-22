0d267f69de07b5d34a1890500f6ca869a3642944
matiasmasjuan <matas.masjuan@uc.cl>
2023-12-21 21:36:29 -0300

    :bust_in_silhouette: Update Crud Client

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -7,28 +7,47 @@ ClientMap(database);
 
 // GET - clients
 router.get('/', async (req: Request, res: Response) => {
-  const result = await Client.findAll();
-  res.status(200).json(result);
+  try {
+    const result = await Client.findAll();
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error getting clients:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // GET - clients/:id
 router.get('/:id', async (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  const result = await Client.findByPk(id);
-  res.status(200).json(result);
+  try {
+    const id = Number(req.params.id);
+    const client = await Client.findByPk(id);
+    if (client) {
+      res.status(200).json(client);
+    } else {
+      res.status(404).json({ error: `No client with the id ${id}` });
+    }
+  } catch (error) {
+    console.error('Error finding the client:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // POST - clients
 router.post('/', async (req: Request, res: Response) => {
-  const { name, rut, salary, savings } = req.body;
-  const result = await Client.create({
-    name,
-    rut,
-    salary,
-    savings
-  });
-  const newClient = result.get({ plain: true }) as Client;
-  res.status(201).json(newClient);
+  try {
+    const { name, rut, salary, savings } = req.body;
+    const result = await Client.create({
+      name,
+      rut,
+      salary,
+      savings
+    });
+    const newClient = result.get({ plain: true }) as Client;
+    res.status(201).json(newClient);
+  } catch (error) {
+    console.error('Error creating the client:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 export default router;
